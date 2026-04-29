913955ca1754388e9d7dd8d5442bdf514dd9efd3
Mitch Dzugan <mitchdzugan@gmail.com>
2026-04-29 03:57:12 -0500

    add nodeOpts

--- default.nix
+++ default.nix
@@ -3,4 +3,10 @@
   version = "1.0.0";
   src = ./.;
   npmDepsHash = "sha256-CQynQAhfiZyNIGLzCadzpuS/WgT5VwXpdqf4RJCixUU=";
+  nativeBuildInputs = [ pkgs.makeWrapper ];
+  postInstall = ''
+    wrapProgram $out/bin/slp-rec \
+      --set NODE_OPTIONS "--experimental-specifier-resolution=node"
+  '';
+
 }
