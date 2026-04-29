742ad0e54b694a1929f692604aa53bbbed1bac32
Mitch Dzugan <mitchdzugan@gmail.com>
2026-04-29 08:23:46 -0500

    handle extensionless via babel build

--- default.nix
+++ default.nix
@@ -3,10 +3,4 @@
   version = "1.0.0";
   src = ./.;
   npmDepsHash = "sha256-T+qxR8M+iy1+PFMRVk1SkQP3J8nI1CccNP5JgPOd8To=";
-  nativeBuildInputs = [ pkgs.makeWrapper ];
-  postInstall = ''
-    wrapProgram $out/bin/slp-rec \
-      --set NODE_OPTIONS "--import=extensionless/register"
-  '';
-
 }
