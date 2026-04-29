9bb7817d1b66052df84d907564e7da373eb9549a
Mitch Dzugan <mitchdzugan@gmail.com>
2026-04-29 08:21:29 -0500

    update deps hash

--- default.nix
+++ default.nix
@@ -2,7 +2,7 @@
   pname = "slp-rec";
   version = "1.0.0";
   src = ./.;
-  npmDepsHash = "sha256-CQynQAhfiZyNIGLzCadzpuS/WgT5VwXpdqf4RJCixUU=";
+  npmDepsHash = "sha256-T+qxR8M+iy1+PFMRVk1SkQP3J8nI1CccNP5JgPOd8To=";
   nativeBuildInputs = [ pkgs.makeWrapper ];
   postInstall = ''
     wrapProgram $out/bin/slp-rec \
