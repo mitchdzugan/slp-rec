98143764227a6439d3449dc63b4d2f35d2e2f224
Mitch Dzugan <mitchdzugan@gmail.com>
2026-05-02 09:02:12 -0500

    update npmDeps hash

--- default.nix
+++ default.nix
@@ -2,5 +2,5 @@
   pname = "slp-rec";
   version = "1.0.0";
   src = ./.;
-  npmDepsHash = "sha256-saUOeKdY+TeS7uui2/fiTxA9pzz8RJopFWA4m8vq9/M=";
+  npmDepsHash = "sha256-ikA8dCxFZckYDOWf2EvIe3tvdfcHz1d64eO4fTrQKYg=";
 }
