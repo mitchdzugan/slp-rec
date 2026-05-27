b54584ca01a21313ff5922305534135ccb95ca23
Mitch Dzugan <mitchdzugan@gmail.com>
2026-05-27 10:03:36 -0500

    fix deps hash

--- default.nix
+++ default.nix
@@ -2,7 +2,7 @@
   pname = "slp-rec";
   version = "1.0.0";
   src = ./.;
-  npmDepsHash = "sha256-FoJYvKAxA60tF2YlfadtKiHK58r5hIdFfLJj9QDBvBc=";
+  npmDepsHash = "sha256-r0AmLEERqsVVloQTuNhIses9Wp96rLbhM6RCqYCSRlc=";
   npmFlags = [ "--legacy-peer-deps" ];
   npmDepsFetcherVersion = 2;
   makeCacheWritable = true;
