4db145561b5a51699053ac14c9bec93b5d8629e4
Mitch Dzugan <mitchdzugan@gmail.com>
2026-05-27 10:02:22 -0500

    try with new stuff

--- default.nix
+++ default.nix
@@ -3,4 +3,7 @@
   version = "1.0.0";
   src = ./.;
   npmDepsHash = "sha256-FoJYvKAxA60tF2YlfadtKiHK58r5hIdFfLJj9QDBvBc=";
+  npmFlags = [ "--legacy-peer-deps" ];
+  npmDepsFetcherVersion = 2;
+  makeCacheWritable = true;
 }
