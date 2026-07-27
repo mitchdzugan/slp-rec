aef0e04efb6269967b30cba8cf14698bb0bc6b20
Mitch Dzugan <mitchdzugan@gmail.com>
2026-07-26 22:40:27 -0500

    better flake tooling

--- flake.nix
+++ flake.nix
@@ -3,30 +3,39 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-root.url = "github:srid/flake-root";
     tsdown.url = "github:mitchdzugan/nix-tsdown";
     spago.url = "github:mitchdzugan/nix-spago";
   };
   outputs = inputs: (
-    inputs.flake-utils.lib.eachDefaultSystem (system: (
-      let
-        pkgs = inputs.nixpkgs.legacyPackages.${system};
-        slp-rec = (pkgs.callPackage ./default.nix { });
-      in {
-        packages = {
-          slp-rec = slp-rec;
-          default = slp-rec;
-        };
-        devShells = {
-          default = pkgs.mkShell {
-            buildInputs = [
-              pkgs.nodejs_26
-              pkgs.purescript
-              inputs.tsdown.packages.${system}.tsdown
-              inputs.spago.packages.${system}.spago
-            ];
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [ inputs.flake-root.flakeModule ];
+      perSystem = { pkgs, system, config, ... }: (
+        let slp-rec = (pkgs.callPackage ./default.nix { }); in {
+          packages = {
+            slp-rec = slp-rec;
+            default = slp-rec;
           };
-        };
-      }
-    ))
+          devShells = {
+            default = pkgs.mkShell {
+              inputsFrom = [ config.flake-root.devShell ];
+              buildInputs = [
+                pkgs.nodejs_26
+                pkgs.purescript
+                inputs.tsdown.packages.${system}.tsdown
+                inputs.spago.packages.${system}.spago
+                (pkgs.writeShellScriptBin "build" ''
+                  cd $FLAKE_ROOT
+                  spago build
+                  tsdown index.js
+                '')
+              ];
+            };
+          };
+        }
+      );
+      systems = inputs.nixpkgs.lib.systems.flakeExposed;
+    }
   );
 }
